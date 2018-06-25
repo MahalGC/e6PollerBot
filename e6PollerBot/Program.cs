0cdc30e25e864e068387f53f3cb242891a635c80
Felix Neko <nekonekofelix@gmail.com>
2018-06-25 03:24:15 -0700

    Include project files.

---
+++ e6PollerBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace e6PollerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
