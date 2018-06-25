09641544f8bbf40706c32a401c90a38305a19b6c
Felix Neko <nekonekofelix@gmail.com>
2018-06-25 04:06:06 -0700

    Started adding code.

--- e6PollerBot/Program.cs
+++ e6PollerBot/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
 
 namespace e6PollerBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().RunBotAsync(args).GetAwaiter().GetResult();
+
+        private static string _botToken;
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            _botToken = args[0];
         }
     }
 }
